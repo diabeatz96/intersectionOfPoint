a7058a099dc0e10fb648ec7e7f88bb92c5ee46cd
diabeatz96 <75499553+diabeatz96@users.noreply.github.com>
2021-10-16 12:12:00 -0400

    Added methods, members, and values to lineType.H. There will be a method to convert a and b to y=mx+b form inorder to make the values of finding a line intersections alot easier. It's the best way since calculating the other revolves around cramer's method, which is more time consuming and difficult to implement in code.

---
+++ algebraQuestion/lineType.h
@@ -0,0 +1,113 @@
+﻿#pragma once
+#include <fstream>
+
+class lineType
+{
+public:
+
+
+
+
+    /*                                    /*
+     * GETTERS AND SETTERS FOR THE CLASS
+     */                                   //
+    
+    double a1() const
+    {
+        return a;
+    }
+
+    void set_a(double a)
+    {
+        this->a = a;
+    }
+
+    double b1() const
+    {
+        return b;
+    }
+
+    void set_b(double b)
+    {
+        this->b = b;
+    }
+
+    double c1() const
+    {
+        return c;
+    }
+
+    void set_c(double c)
+    {
+        this->c = c;
+    }
+
+    double x_value() const
+    {
+        return xValue;
+    }
+
+    void set_x_value(double x_value)
+    {
+        xValue = x_value;
+    }
+
+    double y_value() const
+    {
+        return yValue;
+    }
+
+    void set_y_value(double y_value)
+    {
+        yValue = y_value;
+    }
+
+    double slope1() const
+    {
+        return slope;
+    }
+
+    void set_slope(double slope)
+    {
+        this->slope = slope;
+    }
+
+    double constant1() const
+    {
+        return constant;
+    }
+
+    void set_constant(double constant)
+    {
+        this->constant = constant;
+    }
+
+
+    /*
+     *   Variety of Functions to work with linetypes.
+     *   We will give the user and option to convert in program OR import from a file, either work is fine.
+     *
+     */
+
+    void convertToSlopeForm(double, double, double);
+    bool isVerticalLine(lineType);
+    bool isSameLine(lineType);
+    bool isLineParallel(lineType);
+    bool isLinePerpendicular(lineType);
+    void findIntersection(lineType);
+    void importFileValues(std::fstream);
+    
+    lineType(double, double, double); // CONSTRUCTOR FOR ax + by = c
+    lineType(double, double); //Constructor for y = mx + b
+
+private:
+
+    double a = 0.0; 
+    double b = 0.0;
+    double c = 0.0;
+    
+    double xValue = 0.0;
+    double yValue = 0.0;
+    double slope = 0.0;
+    double constant = 0.0;
+};
